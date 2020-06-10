f2afbc24fd31eb4d5f690e6b0a63e6a124e8b034
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-10 01:25:06 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,41 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+
+
+
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index do |name, index|
+    puts "#{index + 1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |calls|
+    "#{calls.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+def long_planeteer_calls(short_words)
+  if short_words.any?{ |word| word.length > 4}
+    return true
+  elsif short_words.all?{ |word| word.length <= 4}
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+
+
+
+
+def find_the_cheese(array)
+  array.collect do |cheese|
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    if cheese_types.include?(cheese)
+      return cheese
+    end
+  end
+  nil
 end
