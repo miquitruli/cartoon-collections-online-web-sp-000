3025e1e1870f5827a7caede08f142e7baf633278
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-10 00:51:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,13 +5,15 @@
 def roll_call_dwarves(dwarf_names)
   dwarf_names.each_with_index do |name, index|
     puts "#{index + 1} #{name}"
-  end 
+  end
 end
 
 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |calls|
+    return calls.capitalize<<"!"
+  end
 end
 
 
