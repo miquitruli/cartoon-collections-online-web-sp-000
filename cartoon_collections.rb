090b3ba5d760532d45e799004b59761a15f4503f
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-10 01:17:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -36,9 +36,9 @@ end
 def find_the_cheese(array)
   array.collect do |cheese|
     cheese_types = ["cheddar", "gouda", "camembert"]
-    if cheese_types.include?(cheese)
+  if cheese_types.include?(cheese)
       return cheese
-    end
+  end
   end
   nil
 end
